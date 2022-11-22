ee2c0112c63d203b14ac38b921cee3fe11d960af
Danièl Baumann <lokamoka100@gmail.com>
2022-11-22 12:04:14 +0100

    dashboard header

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return render_template('dashboard.html')
 
 if __name__ == '__main__':
