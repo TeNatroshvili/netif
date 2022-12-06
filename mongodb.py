f57998a869943c729f6db5510fe078072a2e8e62
Danièl Baumann <lokamoka100@gmail.com>
2022-12-06 09:27:58 +0100

    new mongostring

--- mongodb.py
+++ mongodb.py
@@ -6,10 +6,3 @@ def get_database():
   
 if __name__ == "__main__":   
     dbname = get_database()
-
-dbname = get_database()
- 
-# Create a new collection
-collection_name = dbname["switches"]
- 
-print(collection_name.get_collection("switches").find())
