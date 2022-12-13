b2c9bff1e8c0a446d50e78eca0888a78f94929c4
ztiefi <l.stiefsohn@htlstp.at>
2022-12-13 12:05:48 +0100

    Switch detection

---
+++ snmpdetectionv3.py
@@ -0,0 +1,41 @@
+from pysnmp.hlapi import *
+import socket
+
+devices = []
+for j in range(255):
+    ip = "10.128.10." + str(j)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0.01)
+    if s.connect_ex((ip, 161)) == 0:
+      devices.append(ip)
+    s.close()
+
+for i in range(255):
+# Replace with the IP address of the switch
+    switch_ip = devices[i]
+    #switch_ip = "10.128.10.19"
+
+# Replace with the SNMP community string
+    community = "public"
+
+# Replace with the OID for the CPU speed
+    name_oid = "1.3.6.1.2.1.1.5.0"
+
+    error_indication, error_status, error_index, var_binds = next(
+        getCmd(SnmpEngine(),
+            CommunityData(community),
+            UdpTransportTarget((switch_ip, 161)),
+            ContextData(),
+            ObjectType(ObjectIdentity(name_oid)))
+    )
+
+    if error_indication:
+     print(error_indication)
+    elif error_status:
+     print('%s at %s' % (error_status.prettyPrint(),
+                          error_index and var_binds[str(error_index) - 1][0] or '?'))
+    else:
+        for var_bind in var_binds:
+        # The CPU speed will be in the first variable binding
+            name = str(var_bind[1])
+            print("the name is " + name)
