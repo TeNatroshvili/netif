7a09a9ae0db96c1e3a345d724e61b19cdea69c34
Danièl Baumann <lokamoka100@gmail.com>
2022-12-13 08:33:13 +0100

    veditor verlinkung

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def dashboard():
     return render_template('dashboard.html')
+
+@app.route('/visualeditor')
+def visualeditor():
+    return render_template('visualeditor.html')
 
 if __name__ == '__main__':
     app.run(debug=1)
