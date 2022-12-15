e6724bc012c96b1211ef4dc13f36999a0f51b75e
ztiefi <l.stiefsohn@htlstp.at>
2022-12-15 10:59:51 +0100

    Switchsuche optimiert

--- snmpdetectionv3.py
+++ snmpdetectionv3.py
@@ -3,7 +3,7 @@ import socket
 
 devices = []
 for j in range(255):
-    ip = "10.128.10." + str(j)
+    ip = "10.128.4." + str(j)
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.settimeout(0.01)
     if s.connect_ex((ip, 161)) == 0:
@@ -14,7 +14,7 @@ for i in range(255):
 # Replace with the IP address of the switch
     switch_ip = devices[i]
     #switch_ip = "10.128.10.19"
-    print(i+1)
+    #print(i+1)
 # Replace with the SNMP community string
     community = "public"
 
@@ -30,12 +30,16 @@ for i in range(255):
     )
 
     if error_indication:
-     print(error_indication)
+     #print(error_indication)
+     #exit
+     print("10.128.4." + str(i) + " -->                                                            nicht vorhanden")
     elif error_status:
-     print('%s at %s' % (error_status.prettyPrint(),
-                          error_index and var_binds[str(error_index) - 1][0] or '?'))
+    # print('%s at %s' % (error_status.prettyPrint(),
+     #                     error_index and var_binds[str(error_index) - 1][0] or '?'))
+      #exit
+      print("10.128.4." + str(i) + " -->                                                            nicht vorhanden")
     else:
         for var_bind in var_binds:
         # The CPU speed will be in the first variable binding
             name = str(var_bind[1])
-            print("the name is " + name)
+            print("10.128.4." + str(i) + " --> " + name)
