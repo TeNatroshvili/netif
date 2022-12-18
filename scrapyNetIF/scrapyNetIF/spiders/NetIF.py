ef7b9d3e5dd55f1689c39b259a5cb040a431e5e3
You Chen <ju.chen@htlstp.at>
2022-12-18 23:34:45 +0100

    alle nötige Informationen scrapbar

--- scrapyNetIF/scrapyNetIF/spiders/NetIF.py
+++ scrapyNetIF/scrapyNetIF/spiders/NetIF.py
@@ -6,6 +6,107 @@ from selenium import webdriver
 import time
 
 
+def getDatasFromSNMPConfPage(response):
+    print("SNMPConf-script----------------")
+    script=response.xpath('//script/text()')[0].extract()
+    scriptText= script.split("var")
+    snmpEnabled=scriptText[2].split('"')[1].split('"')[0]
+    print(snmpEnabled)
+
+def getDatasFromLLDPConfPage(response):
+    print("LLDPConf-script----------------")
+    script=response.xpath('//script/text()')[0].extract()
+    scriptText= script.split("var")
+    lldpEnabledList=scriptText[4].split("(")[1].split(")")[0].split(",")
+    print(lldpEnabledList)
+
+def getDatasFromVlanPortConfPage(response):
+    print("VlanPortConf-script----------------")
+    script=response.xpath('//script/text()')[0].extract()
+    scriptText= script.split("var")
+    vlanEnabled=scriptText[5].split("(")[1].split(")")[0].split(",")
+    packetTypeList=scriptText[6].split("(")[1].split(")")[0].split(",")
+    pvidList=scriptText[7].split("(")[1].split(")")[0].split(",")
+    ingressFilteringList=scriptText[9].split("(")[1].split(")")[0].split(",")
+    print(vlanEnabled)
+    print(packetTypeList)
+    print(pvidList)
+    print(ingressFilteringList)
+
+def getDatasFromLacpSettingPage(response):
+    print("lacp_setting-script----------------")
+    script=response.xpath('//script/text()')[1].extract()
+    scriptText= script.split("var")
+    lacpEnabledList=scriptText[6].split("(")[1].split(")")[0].split(",")
+    lacpEnabledJKeyvalueList=scriptText[7].split("(")[1].split(")")[0].split(",")
+    print(lacpEnabledList)
+    print(lacpEnabledJKeyvalueList)
+
+
+def getDatasFromRateLimitPage(response):
+    print("rate_limit-script----------------")
+    script=response.xpath('//script/text()')[0].extract()
+    scriptText= script.split("var")
+    rateLimitEnabled=scriptText[7].split("(")[1].split(")")[0].split(",")[1]
+    limitIndex=scriptText[6].split("(")[1].split(")")[0].split(",")[2]
+    limit=scriptText[3].split("(")[1].split(")")[0].split(",")[int(limitIndex)]
+    print(rateLimitEnabled)
+    print(limit)
+
+
+def getDatasFromPortConfigurationPage(response):
+    print("port_config-script----------------")
+    script=response.xpath('//script/text()')[0].extract()
+    scriptText= script.split("var")
+    jumbo_enabled=scriptText[6].split("(")[1].split(")")[0].split(",")
+    if not bool(jumbo_enabled):
+        jumbo_enabled="checked"
+    else:
+        jumbo_enabled="unchecked"
+    print(jumbo_enabled)
+
+def getDatasFromPortsMirrorPage(response):
+    print("port_mirror-script----------------")
+    script=response.xpath('//script/text()')[0].extract()
+    scriptText= script.split("var")
+    port_to_mirror_to=scriptText[1].split('"')[1].split('"')[0]
+    ports_to_mirrorList=scriptText[2].split("(")[1].split(")")[0].split(",")
+    print(port_to_mirror_to)
+    print(ports_to_mirrorList)
+
+def getDatasFromInformationPage(response):
+                print("infomation-script----------------")
+                script=response.xpath('//script/text()')[0].extract()
+                scriptText= script.split("var")
+                 
+               # linksStatusList=scriptText[3].split("(").split
+                system_name=response.xpath('/html/body/form/table//tr[1]/td/table//tr[3]/td/table[1]//tr[2]/td[2]/text()').extract()
+                serial_number=response.xpath('/html/body/form/table//tr[1]/td/table//tr[3]/td/table[1]//tr[8]/td[2]/text()').extract()
+                ip_adresse=response.xpath('/html/body/form/table//tr[1]/td/table//tr[3]/td/table[1]//tr[12]/td[2]/text()').extract()
+                subnet_mask=response.xpath('/html/body/form/table//tr[1]/td/table//tr[3]/td/table[1]//tr[13]/td[2]/text()').extract()
+                gateway_ip=response.xpath('/html/body/form/table//tr[1]/td/table//tr[3]/td/table[1]//tr[14]/td[2]/text()').extract()
+                mac_adreese=response.xpath('/html/body/form/table//tr[1]/td/table//tr[3]/td/table[1]//tr[15]/td[2]/text()').extract()
+                print(system_name," ", serial_number," ",ip_adresse," ",
+                subnet_mask," ",gateway_ip," ",mac_adreese)
+               
+                linksStatusList=scriptText[4].split("(")[1].split(")")[0].split(",")
+                portsSpeedList=scriptText[5].split("(")[1].split(")")[0].split(",")
+                flowControlStatusList=scriptText[6].split("(")[1].split(")")[0].split(",")
+                autoNegotiationList=scriptText[7].split("(")[1].split(")")[0].split(",")
+                frameTypeList=scriptText[8].split("(")[1].split(")")[0].split(",")
+                pvidList=scriptText[9].split("(")[1].split(")")[0].split(",")
+                trunkNameList=scriptText[10].split("(")[1].split(")")[0].split(",")
+                trunkMemberList=scriptText[11].split("(")[1].split(")")[0].split(",")
+                
+                print(linksStatusList)
+                print(portsSpeedList)
+                print(flowControlStatusList)
+                print(autoNegotiationList)
+                print(frameTypeList)
+                print(pvidList)
+                print(trunkNameList)
+                print(trunkMemberList)
+
 class DemoySpider(scrapy.Spider):
     name = 'NetIF'
    # allowed_domains = ['www.youtube.com']
@@ -16,9 +117,9 @@ class DemoySpider(scrapy.Spider):
 
     def start_requests(self):
         urls = ['http://10.128.10.19/status/status_ov.html',
+                'http://10.128.10.19/ports/ports_bsc.html',
                 'http://10.128.10.19/ports/ports_config.html',
                 'http://10.128.10.19/ports/ports_mir.html',
-                'http://10.128.10.19/trunks/trunks_mem.html',
                 'http://10.128.10.19/trunks/lacp.html',
                 'http://10.128.10.19/vlans/vlan_pconf.html',
                 'http://10.128.10.19/vlans/vlan_mconf.html',
@@ -34,9 +135,34 @@ class DemoySpider(scrapy.Spider):
         option.add_experimental_option('excludeSwitches', ['enable-authmation'])
         ser = Service("./chromedriver.exe")
         self.bro = webdriver.Chrome(options=option,service=ser)
+    
+    
 
     def parse(self, response):
-        print(response.xpath('//td[@class="page_title"]/text()')[0].extract())
+        page_title=response.xpath('//td[@class="page_title"]/text()')[0].extract()
+        print(page_title)
+        match page_title:
+            case "System Information":
+                getDatasFromInformationPage(response)
+            case "Rate Limits":
+                getDatasFromRateLimitPage(response)
+            case "Port Configuration":
+                getDatasFromPortConfigurationPage(response)
+            case "Port Mirroring":
+                getDatasFromPortsMirrorPage(response)
+            case "LACP Setting":
+                getDatasFromLacpSettingPage(response)
+            case "802.1Q Per Port Configuration":
+                getDatasFromVlanPortConfPage(response)
+            case "LLDP Configuration":
+                getDatasFromLLDPConfPage(response)
+            case "SNMP Configuration":
+                getDatasFromSNMPConfPage(response)
+                
+
+
+    
+
 
         
 
