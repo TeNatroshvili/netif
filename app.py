e4b2d9445f8b61e3d5713d8f2201fb4acef86845
Danièl Baumann <lokamoka100@gmail.com>
2023-01-17 11:34:57 +0100

    switch list

--- app.py
+++ app.py
@@ -2,19 +2,11 @@ from flask import Flask, render_template, request
 import json
 import scrapy
 from scrapyNetIF.scrapyNetIF.spiders.NetIF import postsomeThing
+from flask import request,redirect
+import requests
+
+from mongodb import switches
 app = Flask(__name__)
-
-
-
-@app.route('/setRateLimit',methods=['Get','POST'])
-def set_rate_limit():
-    print("requestdata")
-    print(request.args)
-    login_data={"password":"Syp2223"}
-    form_data={"_submit":"Apply","R11":"2","R52":"on","R12":"2","R51":"1"}
-    postsomeThing(form_data)
-    return "done"
-    
 
 @app.route('/')
 def dashboard():
@@ -22,7 +14,7 @@ def dashboard():
 
 @app.route('/visualeditor')
 def visualeditor():
-    return render_template('visualeditor.html')
+    return render_template('visualeditor.html', switches = switches.find())
 
 if __name__ == '__main__':
     app.run(debug=1)
