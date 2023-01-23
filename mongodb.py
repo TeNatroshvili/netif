09d0287585426b5a84927a95c40832d2d88214dd
Danièl Baumann <lokamoka100@gmail.com>
2023-01-23 11:27:59 +0100

    optimmiized

--- mongodb.py
+++ mongodb.py
@@ -8,6 +8,3 @@ switches = collection["switches"]
 #switches.insert_one({ "name": "test 3", "address": "Highway 37" })
 #switches.delete_many({ "name": "test 3", "address": "Highway 37" })
 #switches.delete_many({})
-
-for x in switches.find():
-  print(x)
